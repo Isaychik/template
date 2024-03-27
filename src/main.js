3aa76d7e78510d6aff4f2ef78f1863195f361ce7
Isay <isay2005u@mail.ru>
2024-03-27 22:50:02 +0300

    Установил все необходимые инструменты для начала: tailwind, axios, pinit-persist, iconify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import './assets/main.css'
+import { Icon } from '@iconify/vue'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.component('Icon', Icon)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
